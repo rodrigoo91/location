fb6123278af5d98b8a6a3b93436f245ed2be0fb6
rodrigoo91 <150820790+rodrigoo91@users.noreply.github.com>
2024-11-13 15:46:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,20 +14,8 @@ app.use(bodyParser.json());
 // Servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Conexão com MongoDB
-mongoose.connect('mongodb://localhost:27017/localizacao', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Definição do esquema e modelo
-const locationSchema = new mongoose.Schema({
-    latitude: Number,
-    longitude: Number,
-    timestamp: String
-});
-
-const Location = mongoose.model('Location', locationSchema);
-
 // Rota para receber dados de localização
-app.post('/api/location', (req, res) => {
+app.post('https://location-89fe.onrender.com/', (req, res) => {
     const newLocation = new Location(req.body);
     newLocation.save()
         .then(() => res.json({ message: 'Localização armazenada com sucesso!' }))
@@ -35,7 +23,7 @@ app.post('/api/location', (req, res) => {
 });
 
 // Rota para acessar os dados armazenados
-app.get('/api/locations', (req, res) => {
+app.get('https://location-89fe.onrender.com/', (req, res) => {
     Location.find()
         .then(locations => res.json(locations))
         .catch(err => res.status(400).json({ error: err.message }));
